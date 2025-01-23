733fec09ada51c0005f78d6e2e11161fec672028
szczuras7 <szczuras7@proton.me>
2025-01-23 17:13:57 +0100

    Klasy i test połączenia

---
+++ WpfApp001/Classes/Update.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp001.Classes
+{
+    internal class Update
+    {
+        private string connectionString = "Server=projekt-interfejsy.mysql.database.azure.com;Database=szpital;Uid=szczuras;Pwd=Interfejsy123;SslMode=Required;";
+        public void updatePacjenci()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MessageBox.Show("Connection Open!");
+            }
+
+        }
+    }
+}
