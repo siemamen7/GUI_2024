a4de794d4df3c2a06cdb1484da6c064de389b4ba
szczuras7 <szczuras7@proton.me>
2025-01-26 22:20:02 +0100

    Wersja bez mergów

--- WpfApp001/EntityFramework/szpitalContext.cs
+++ WpfApp001/EntityFramework/szpitalContext.cs
@@ -47,7 +47,6 @@ public partial class szpitalContext : DbContext
     public virtual DbSet<Wydarzenium> Wydarzenia { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseMySql("server=projekt-interfejsy.mysql.database.azure.com;database=szpital;userid=szczuras;password=Interfejsy123;sslmode=Required", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.39-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
